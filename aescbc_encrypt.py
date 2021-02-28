a901db991e14d2ce0cf049faf7453ecb52b87386
Jo-Nah <jonahtham@u.nus.edu>
2021-02-28 15:15:41 +0800

    Added line to convert base64 to hex

--- aescbc_encrypt.py
+++ aescbc_encrypt.py
@@ -7,9 +7,9 @@ from Crypto.Util.Padding import pad, unpad
 # Run 'python3 aescbc_encrypt.py > ciphertext.json'
 
 # Set up key, plaintext and iv
-key = bytearray.fromhex('3ca7506212e8373b83b1da2ae6b291b995d4ad9a2220cd6f214bc18742d36882')
-plaintext = "Coding crypto is really fun".encode()
-iv = bytearray.fromhex('64833f8b4fa2da33b39ec9a48345a458')
+key = bytearray.fromhex('ca64e7ca9072688df97616cf67417ea0e5d7d3470ca34037fb3ae7dfd750ad75')
+plaintext = "I really really hate crypto".encode()
+iv = bytearray.fromhex('6de5f9be2d8349d726fb54d0742dfbdb')
 
 # Cipher object to use
 cipher = AES.new(key, AES.MODE_CBC, iv=iv)
@@ -18,7 +18,12 @@ cipher = AES.new(key, AES.MODE_CBC, iv=iv)
 data = cipher.encrypt(pad(plaintext, AES.block_size))
 
 iv = b64encode(cipher.iv).decode('utf-8')
+# ciphertext = b64encode(data).decode('utf-8')
 ciphertext = b64encode(data).decode('utf-8')
 # Save iv and ciphertext in json
 iv_ciphertext_json = json.dumps({'iv':iv, 'ciphertext':ciphertext})
-print(iv_ciphertext_json) 
+print(iv_ciphertext_json)
+
+print("\nOutput in hex:")
+decode_text = b64decode(ciphertext).hex()
+print(decode_text)
